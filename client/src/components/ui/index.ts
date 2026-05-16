5c0114336c391914d3294f037b5bacab55575ffd
Tanvir-Ahmed-Apu <ahmed010tanvir@gmail.com>
2026-05-16 10:18:47 +0600

    refactor: remove PokemonEmptyState component and its associated CSS file

--- client/src/components/ui/index.ts
+++ client/src/components/ui/index.ts
@@ -1,13 +1,11 @@
 import ContentCard from "./ContentCard";
 import OAuthPopupCallback from "./OAuthPopupCallback";
-import PokemonEmptyState from "./PokemonEmptyState";
 import SimpleEmojiPicker from "./SimpleEmojiPicker";
 import ThemeToggle from "./ThemeToggle";
 
 export {
   ContentCard,
   OAuthPopupCallback,
-  PokemonEmptyState,
   SimpleEmojiPicker,
   ThemeToggle
 };
