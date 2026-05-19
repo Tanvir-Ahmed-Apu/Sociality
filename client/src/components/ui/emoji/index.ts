ecdcbbdf4093631db6da5f940c6e38cdd2b3e620
Tanvir-Ahmed-Apu <ahmed010tanvir@gmail.com>
2026-05-19 22:01:09 +0600

    refactor: modularize emoji data into separate category files and clean up SimpleEmojiPicker implementation

---
+++ client/src/components/ui/emoji/index.ts
@@ -0,0 +1,13 @@
+import { SMILEYS_EMOJIS } from './smileys';
+import { ANIMALS_EMOJIS } from './animals';
+import { FOOD_EMOJIS } from './food';
+import { ACTIVITIES_EMOJIS } from './activities';
+import { SYMBOLS_EMOJIS } from './symbols';
+
+export const EMOJI_LIST = [
+  ...SMILEYS_EMOJIS,
+  ...ANIMALS_EMOJIS,
+  ...FOOD_EMOJIS,
+  ...ACTIVITIES_EMOJIS,
+  ...SYMBOLS_EMOJIS,
+];
